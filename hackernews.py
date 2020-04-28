f4f9106b476bcfe982ad3f6efa3626167a5ed7fc
Victor MEUNIER <vmeunier@signoptic.local>
2020-04-28 10:53:39 +0200

    add helper functions

--- hackernews.py
+++ hackernews.py
@@ -2,14 +2,24 @@ import requests
 
 url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
 
-r = requests.get(url)
 
-data = r.json()
-
-print("{} stories".format(len(data)))
-
-for (i, id) in enumerate(data):
-    url = "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty".format(id)
+def get_top_stories():
     r = requests.get(url)
     data = r.json()
-    print("Title: {} - by: {} - score: {} - URL: {}".format(data['title'], data['by'], data['score'], data['url']))
+    return data
+
+def get_story_from_id(id):
+    url = "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty".format(id)
+    r = requests.get(url)
+    return r.json()
+
+
+if __name__ == "__main__":
+    data = get_top_stories()
+    print("{} stories".format(len(data)))
+
+    for (i, id) in enumerate(data):
+        url = "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty".format(id)
+        r = requests.get(url)
+        data = r.json()
+        print("Title: {} - by: {} - score: {} - URL: {}".format(data['title'], data['by'], data['score'], data['url']))
